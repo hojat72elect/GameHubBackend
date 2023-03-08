02ee4e7fceb41a53499b7807ef677cb52a041b78
hojat <hojat72elect@gmail.com>
2023-03-07 23:05:07 -0500

    4th iteration of account extractor

--- bots/telegram/db/AccountsExtractor.py
+++ bots/telegram/db/AccountsExtractor.py
@@ -52,27 +52,27 @@ for g in groups:
     i += 1
 
 # now we extract users from these groups, one by one.
-print("fetching members")
-group = groups[2]
-allParticipants = []
-allParticipants = client.get_participants(group, aggressive=True)
+# print("fetching members")
+# group = groups[2]
+# allParticipants = []
+# allParticipants = client.get_participants(group, aggressive=True)
 
-print('Saving In file...')
-with open("telegram_accounts.csv", "a", encoding='UTF-8') as f:
-    writer = csv.writer(f, delimiter=",", lineterminator="\n")
-    writer.writerow(['user id', 'access hash', 'name', 'group', 'group id'])
-    for user in allParticipants:
-        if user.username:
-            username = user.username
-        else:
-            username = ""
-        if user.first_name:
-            first_name = user.first_name
-        else:
-            first_name = ""
-        if user.last_name:
-            last_name = user.last_name
-        else:
-            last_name = ""
-        name = (first_name + ' ' + last_name).strip()
-        writer.writerow([username, user.id, user.access_hash, name, group.title, group.id])
+# print('Saving In file...')
+# with open("telegram_accounts.csv", "a", encoding='UTF-8') as f:
+#     writer = csv.writer(f, delimiter=",", lineterminator="\n")
+#     writer.writerow(['user id', 'access hash', 'name', 'group', 'group id'])
+#     for user in allParticipants:
+#         if user.username:
+#             username = user.username
+#         else:
+#             username = ""
+#         if user.first_name:
+#             first_name = user.first_name
+#         else:
+#             first_name = ""
+#         if user.last_name:
+#             last_name = user.last_name
+#         else:
+#             last_name = ""
+#         name = (first_name + ' ' + last_name).strip()
+#         writer.writerow([username, user.id, user.access_hash, name, group.title, group.id])
