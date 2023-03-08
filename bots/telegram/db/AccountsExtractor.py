ccfa527ee0075572504045275b7a0bb391e50f98
hojat <hojat72elect@gmail.com>
2023-03-07 22:13:59 -0500

    2 iterations fo account extractor

--- bots/telegram/db/AccountsExtractor.py
+++ bots/telegram/db/AccountsExtractor.py
@@ -1,3 +1,5 @@
+import csv
+
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
@@ -38,7 +40,7 @@ chats.extend(result.chats)
 
 for chat in chats:
     try:
-        if chat.megagroup or chat.broadcast:
+        if chat.megagroup:
             groups.append(chat)
     except:
         continue
@@ -48,3 +50,29 @@ i = 0
 for g in groups:
     print(str(i) + '- ' + g.title)
     i += 1
+
+# now we extract users from these groups, one by one.
+# print("fetching members")
+# group = groups[1]
+# allParticipants = []
+# allParticipants = client.get_participants(group, aggressive=True)
+#
+# print('Saving In file...')
+# with open("telegram_accounts.csv", "a", encoding='UTF-8') as f:
+#     writer = csv.writer(f, delimiter=",", lineterminator="\n")
+#     writer.writerow(['user id', 'access hash', 'name', 'group', 'group id'])
+#     for user in allParticipants:
+#         if user.username:
+#             username = user.username
+#         else:
+#             username = ""
+#         if user.first_name:
+#             first_name = user.first_name
+#         else:
+#             first_name = ""
+#         if user.last_name:
+#             last_name = user.last_name
+#         else:
+#             last_name = ""
+#         name = (first_name + ' ' + last_name).strip()
+#         writer.writerow([username, user.id, user.access_hash, name, group.title, group.id])
