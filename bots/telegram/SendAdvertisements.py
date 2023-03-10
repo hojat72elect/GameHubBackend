2924452e979030c00b177bdb4f82b8fbdb6d16b4
hojat <hojat72elect@gmail.com>
2023-03-10 17:52:37 -0500

    initial implementation of telegram bot

---
+++ bots/telegram/SendAdvertisements.py
@@ -0,0 +1,7 @@
+"""
+This is going to be the main bot that sends advertisements to Telegram users.
+"""
+
+
+# read the CSV
+
--- bots/telegram/SendMessageInTelegram.py
+++ bots/telegram/SendMessageInTelegram.py
@@ -1,23 +1,30 @@
 from telethon import TelegramClient
 import asyncio
+
+from telethon.tl.types import InputPeerUser
+
 from bots.tokens import TELEGRAM_API_ID, TELEGRAM_API_HASH
 
 """
-Don't pay attention to other files in this directory, this is the correct way to send a message in python."""
+Don't pay attention to other files in this directory, 
+this is the correct way to send a message in python.
+"""
 
 
 async def sendMessage() -> asyncio.coroutine:
     client = TelegramClient('session_name', TELEGRAM_API_ID, TELEGRAM_API_HASH)
     await client.start()
 
+    receiver = InputPeerUser(user_id=2005055437, access_hash=-300479617109090590)
+
     # print(client.get_me().stringify())  # just returns info about myself
 
     # the message to be sent
-    message = f"Hey there! 👋\n This is an Android app for following all the latest news about video games; I promise " \
+    message = f"Hey there! 👋\nThis is an Android app for following all the latest news about video games; I promise " \
               f"you'll enjoy it a lot:\nhttps://play.google.com/store/apps/details?id=ca.on.hojat.gamenews"
 
     # All the recipients
-    await client.send_message('hojat72elect', "don't tickle a sleeping dragon!")
+    await client.send_message(receiver, message)
 
 
 asyncio.run(sendMessage())
